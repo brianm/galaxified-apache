5a62ad5bb35e8532996654bc4fc0b95608fb3693
Brian McCallister <brianm@skife.org>
2012-02-19 20:19:08 -0700

    better string manipulation in startup -- bash3 compat

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 PORT="8000"
-
+echo $(which bash)
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+echo "ROOT   $ROOT"
 
 for source in $(find $ROOT/etc -type f) 
 do
-  target="${ROOT}/${source//"${ROOT}/etc"}"
+  echo "SOURCE $source"
+  target="$ROOT/${source#"$ROOT/etc/"}"
+  echo "INITIT $target"
   if [[ $target =~ \.m4$ ]] 
   then
     extension="m4"
-    target=${target//.m4}
+    target=${target%".m4"}
   else
     extension=""
   fi
+  echo "TARGET $target"
+  echo "--"
+
   case $extension in
     m4 )
-      echo "m4 processing $source"
+      #echo "m4 processing $source"
       m4 "-DGXY_PORT=$PORT" "-DGXY_DEPLOY_ROOT=$ROOT" "$source" > "$target"
       ;;
     * )
@@ -25,3 +31,4 @@ do
 done
 
 $ROOT/httpd/bin/apachectl start
+
